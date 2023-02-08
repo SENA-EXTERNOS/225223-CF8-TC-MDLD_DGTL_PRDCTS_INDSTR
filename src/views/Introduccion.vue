4a7f0186ec5904a198e59ea68500296c0d05f8e0
ricardog07 <71521082+ricardog07@users.noreply.github.com>
2023-02-07 20:13:37 -0500

    Se vinculan los videos.

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -19,8 +19,8 @@
   
       figure.mb-5
         .video
-          iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-        figcaption Video: Leyenda del video
+          iframe(width="560" height="315" src="https://www.youtube.com/embed/i3vh-h_Jqu0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        figcaption Video: Manufactura aditiva
 
 </template>
 
